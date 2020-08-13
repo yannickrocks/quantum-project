37f7d8c75b2b047f74337300a22caa5e2ca51aa8
Yannick Mermet <yannickmermet@hotmail.com>
2020-08-13 18:08:07 +0100

    Images are there for puzzle 1

--- src/screens/puzzle1/index.tsx
+++ src/screens/puzzle1/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Background from 'src/assets/background.png';
+import Minutes22 from 'src/assets/22mins-top_web.png';
+import './puzzle1.css';
 
 const Puzzle1: React.FC = () => {
     return (
         <div>
-            <figure>
-                <img src={Background} alt="Background" />
+            <img className="background" src={Background} alt="Background" />
+            <figure className="puzzle1">
+                <img className="puzzle1__text" src={Minutes22} alt="22 minutes" />      
             </figure>
+            {/* Make Video Emded */}
+            {/*Add Planets and input boxes  */}
+            {/* Add buttons */}
         </div>
     )
 };
