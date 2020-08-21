9fcbdd1dd5c3a30dba49c814d626f1b84caeb73e
Yannickrocks <yannickmermet@hotmail.com>
2020-08-21 16:17:58 +0200

    updated wrong import

--- src/screens/puzzle1/index.tsx
+++ src/screens/puzzle1/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Container from 'react-bootstrap/container';
+import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Figure from 'react-bootstrap/Figure';
