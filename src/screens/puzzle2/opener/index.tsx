9d9d62da201f4895903a68582f0f076ff01eeff3
Yannick Mermet <yannickmermet@hotmail.com>
2020-11-07 16:22:00 +0000

    Changed buttons and CSS

--- src/screens/puzzle2/opener/index.tsx
+++ src/screens/puzzle2/opener/index.tsx
@@ -1,41 +1,39 @@
 import React from "react";
 import FadeIn from "react-fade-in";
-import Button from "react-bootstrap/Button";
 import Figure from "react-bootstrap/Figure";
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Background from "src/assets/Misc/background.png";
+import Background from "src/assets/Misc/backgroundZaps.png";
 import Mask from "src/assets/Misc/Mask.png";
-import "../puzzle2.css";
+import "./opener1.css";
 
 const Puzzle2Opener: React.FC = () => {
+  const backgroundStyle = {
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    backgroundImage: `url(${Background})`,
+    backgroundColor: "black",
+    width: "100vw",
+    height: "100vh",
+  };
+
+  const containerStyle = {
+    width: "700px",
+  };
+
   return (
-    <>
-      <img className="background" src={Background} alt="Background" />
+    <div className="puzzle2__opener" style={backgroundStyle}>
       <FadeIn delay={600} transitionDuration={1000}>
-        <Container fluid className="p-wanderingmoon">
-          <Row className="justify-content-md-center">
-            <Col xs={6} md={4}>
-              <Figure className="puzzle2__Mask">
-                <Figure.Image src={Mask} alt="Mask Puzzle 2"></Figure.Image>
-              </Figure>
-            </Col>
-          </Row>
-          <Row className="justify-content-md-center">
-            <Col xs={5} md={3}>
-              <Button
-                className="puzzle2__wakeUp"
-                type="input"
-                href="/wanderingmoon"
-              >
-                Wake up
-              </Button>
-            </Col>
-          </Row>
+        <Container fluid className="puzzle2__container" style={containerStyle}>
+          <Figure className="puzzle2__mask">
+            <Figure.Image src={Mask} alt="Mask Puzzle 2" />
+          </Figure>
+          <a className="puzzle2__wakeUp" href="/wanderingmoon">
+            Wake up
+          </a>
         </Container>
       </FadeIn>
-    </>
+    </div>
   );
 };
 
--- src/screens/puzzle3/opener/index.tsx
+++ src/screens/puzzle3/opener/index.tsx
@@ -1,41 +1,39 @@
 import React from "react";
 import FadeIn from "react-fade-in";
-import Button from "react-bootstrap/Button";
 import Figure from "react-bootstrap/Figure";
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Background from "src/assets/Misc/background.png";
+import Background from "src/assets/Misc/backgroundZaps.png";
 import Mask from "src/assets/Misc/Mask.png";
-import "../puzzle3.css";
+import "./opener2.css";
 
 const Puzzle3Opener: React.FC = () => {
+  const backgroundStyle = {
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    backgroundImage: `url(${Background})`,
+    backgroundColor: "black",
+    width: "100vw",
+    height: "100vh",
+  };
+
+  const containerStyle = {
+    width: "700px",
+  };
+
   return (
-    <>
-      <img className="background" src={Background} alt="Background" />
+    <div className="puzzle3__opener" style={backgroundStyle}>
       <FadeIn delay={600} transitionDuration={1000}>
-        <Container fluid className="finalVoyage">
-          <Row className="justify-content-md-center">
-            <Col xs={6} md={4}>
-              <Figure className="puzzle3__Mask">
-                <Figure.Image src={Mask} alt="Mask Puzzle 2"></Figure.Image>
-              </Figure>
-            </Col>
-          </Row>
-          <Row className="justify-content-md-center">
-            <Col xs={5} md={3}>
-              <Button
-                className="puzzle3__wakeUp"
-                type="input"
-                href="/finalvoyage"
-              >
-                Wake up
-              </Button>
-            </Col>
-          </Row>
+        <Container fluid className="puzzle3__container" style={containerStyle}>
+          <Figure className="puzzle3__mask">
+            <Figure.Image src={Mask} alt="Mask Puzzle 3"></Figure.Image>
+          </Figure>
+          <a className="puzzle3__wakeUp" href="/finalvoyage">
+            Wake up
+          </a>
         </Container>
       </FadeIn>
-    </>
+    </div>
   );
 };
 
