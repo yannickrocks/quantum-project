d12114eee98a9439a7e57d73e323e5ebc3cfba31
Yannick Mermet <yannickmermet@hotmail.com>
2020-12-09 14:34:11 +0000

    Update Texts

--- src/assets/Texts/Constants.tsx
+++ src/assets/Texts/Constants.tsx
@@ -1,10 +1,10 @@
 const HomePageText1 =
   "Congratulations on becoming Outer Wild's Ventures' newest astronaut.";
 const HomePageText2 =
-  "You have everything you need to begin your journey, and don't forget to use the tools you have at your disposal. This site's code contains the answers you need by necessity, but to progression of that sort would rob you of the lessons you learn along the way.";
+  "You already have everything you need to complete your journey, using the tools you have at your disposal. Completing the Outer Wilds isn’t required for this experience, but said experience will be directly enriched if you have, and we really recommend it. This site's HTML code may contain the answers you need by necessity, but progression of that sort would rob you of the lessons you learn along the way.";
 const HomePageText3 =
-  "Finally, this was made by fans, for fans. We all adore the outerwilds, but we appreciate that this project uses ideas and content that we don't own any of the right to. We hope that those in charge enjoy this enough to let that one slide.";
-const HomePageText4 = "Good Luck.";
+  "Finally, this was made by fans, for fans. We adore the Outer Wilds, but we appreciate that this project uses ideas and content that we don't own any of the rights to. We hope that those in charge enjoy this for the passion project it has been, and view it with indulgence.";
+const HomePageText4 = "Clocks’ ticking, hatchling. Good Luck.";
 export const HomePageText = [
   HomePageText1,
   HomePageText2,
@@ -14,15 +14,15 @@ export const HomePageText = [
 
 const Minutes22Text1 = "Chert's Research Notes - property of Chert!";
 const Minutes22Text2 =
-  "I met our newest astronaut today. They seem nice, if a little odd. For example: they said they heard Feldspar's harmonica coming from Dark Bramble and then casually followed it up with 'the universe is ending' like it wasn't even news to them!";
+  "I met our newest astronaut today. They seem nice - if a little odd. For example: earlier they said they heard Feldspar's harmonica coming from Dark Bramble and then casually followed it up with 'the universe is ending' like it wasn't even a big deal to them.";
 const Minutes22Text3 =
-  "Thankfully, I know that isn't the case. I would have seen the signs; like, oh I don't know.. the sun turning red. Or more supernovae than usual. Like that one there! and that one, ...and that one. Maybe I should check my notes.";
+  "Thankfully, I know that isn't the case. I would have seen the signs; like, oh I don't know... the sun turning red. Or more supernovae than usual. Like that one! or that one. ...and that one. Maybe I should check my notes.";
 export const Minutes22Text = [Minutes22Text1, Minutes22Text2, Minutes22Text3];
 
 const WanderingMoonText1 =
-  "How curious, there appears to be a celestial body that roams freely amongst its friends in this solar system.";
+  "How curious! There appears to be a celestial body that roams freely amongst its friends in this solar system.";
 const WanderingMoonText2 =
-  "How refreshing it is to find new anomalies to study along our nomadic journey... provided, that is, we can work around its apparent natural state of a quantum superposition.";
+  "How refreshing it is to find new anomalies to study along our nomadic journey... provided, that is, we can work around its’ apparent natural state of a quantum superposition.";
 const WanderingMoonText3 = "Refreshing, yes. Straightforward, not so much.";
 export const WanderingMoonText = [
   WanderingMoonText1,
@@ -37,9 +37,9 @@ export const WanderingMoonAlmostCorrect =
   "Tracking Information correct but location of quantum moon remains unknown";
 
 const FinalVoyageText1 =
-  "The ancient machines' warp core rests in your hand. You know where you need to take it, but there's only a limited time to ascertain your true destination's coordinates.";
+  "The ancient machines' warp core rests in your hand. You know where you need to take it, but as always, there's only a limited time to ascertain your true, final destination's coordinates.";
 const FinalVoyageText2 =
-  "The immediate journey will be treacherous, and with no safety net beneath you, you have this one last loop to get it right.";
+  "The immediate journey will be treacherous, and with no safety net beneath you, you only have this one last loop to get it right.";
 const FinalVoyageText3 = "You can do this. Good luck.";
 
 export const FinalVoyageText = [
